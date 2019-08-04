9a957a0237daa2997f411b95fb1a5bf3c450ee97
Cotescu Victor Andrei <53575824+P93VA@users.noreply.github.com>
2019-08-04 16:08:50 -0700

    Am inceput

---
+++ Septica/Septica/Assets/Scripts/Cartejuc.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Cartejuc : MonoBehaviour
+{
+
+
+
+    public GameObject pachetel;
+    Pachet pachet;
+
+    
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        pachet = pachetel.GetComponent<Pachet>();
+
+        pachet.shuffle();
+
+
+        int[,] pachet2 = new int[32,2];
+
+        int ValIncr, CulIncr;
+        for (int i = 0; i < 33; i++)
+        {
+            ValIncr = pachet.ReturnCardVal(i);
+            for (int j = 0; j < 4; j++)s
+            {
+
+
+                CulIncr = pachet.ReturnCardCol(i);
+                pachet2[i,j] = { { ValIncr, CulIncr} }; 
+        }
+        }
+
+        
+       
+
+        gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load("c01", typeof(Sprite)) as Sprite;
+        string nume = gameObject.GetComponent<SpriteRenderer>().sprite.texture.name;
+        transform.localScale = new Vector3(0.35f, 0.35f);
+        
+        int carte = 0;
+
+        Int32.TryParse(nume, out carte);
+
+        Console.WriteLine(carte);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Septica/Septica/Assets/Scripts/Pachet.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Linq;
+using System;
+
+
+public class Pachet : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int RandomNumber(int min, int max)
+    {
+        System.Random random = new System.Random();
+        return random.Next(min, max);
+    }
+
+        public Tuple<int, int>[] pachet =
+           {
+                     Tuple.Create(7,1),
+                     Tuple.Create(7,2),
+                     Tuple.Create(7,3),
+                     Tuple.Create(7,4),
+                     Tuple.Create(8,1),
+                     Tuple.Create(8,2),
+                     Tuple.Create(8,3),
+                     Tuple.Create(8,4),
+                     Tuple.Create(9,1),
+                     Tuple.Create(9,2),
+                     Tuple.Create(9,3),
+                     Tuple.Create(9,4),
+                     Tuple.Create(10,1),
+                     Tuple.Create(10,2),
+                     Tuple.Create(10,3),
+                     Tuple.Create(10,4),
+                     Tuple.Create(11,1),
+                     Tuple.Create(11,2),
+                     Tuple.Create(11,3),
+                     Tuple.Create(11,4),
+                     Tuple.Create(12,1),
+                     Tuple.Create(12,2),
+                     Tuple.Create(12,3),
+                     Tuple.Create(12,4),
+                     Tuple.Create(13,1),
+                     Tuple.Create(13,2),
+                     Tuple.Create(13,3),
+                     Tuple.Create(13,4),
+                     Tuple.Create(14,1),
+                     Tuple.Create(14,2),
+                     Tuple.Create(14,3),
+                     Tuple.Create(14,4)
+    };
+
+
+    public void shuffle()
+    {
+        //Console.WriteLine(pachet[0]);
+        int prev = 0;
+        for (int i = 0; i < 33; i++)
+        {
+            e: int a = RandomNumber(0, 32);
+
+            if (a == i || a == prev) goto e;
+
+            prev = a;
+
+            (pachet[i], pachet[a]) = (pachet[a], pachet[i]);
+        }
+    }
+
+    public int ReturnCardVal(int i)
+    {
+            return pachet[i].Item1;
+
+    }
+
+    public int ReturnCardCol(int i)
+    {
+
+            return pachet[i].Item2;
+
+    }
+
+    void Start()
+    {
+
+        shuffle();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
